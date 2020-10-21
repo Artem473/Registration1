4a6229de1e740880298a51a10a9986139e8497af
Max <gsx401@yandex.ru>
2020-10-21 09:57:19 +0300

    git remote add origin https://github.com/Artem473/-.git git branch -M main git push -u origin main

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,23 +1,38 @@
 package sample;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Model {
-    public void reg(String login, String pass) throws IOException {
-        File file = new File("C:/Java 42т/1/Registration.txt");
-        file.createNewFile();
+    public void reg(String login, String pass , Label status) throws IOException {
+        File file = new File("C:/Java_42/1/Registration.txt");
         FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("Login: " + login + "\n" + "Password: " + pass);
+        file.createNewFile();
+        if (login .isEmpty() || pass.isEmpty()) {
+            status.setText("Введите логин или пароль");
+        }else status.setText("Вы зарегистрированы.");
+        fileWriter.write(login +"\n" + pass);
         fileWriter.close();
-    }
-    public void sign(String login, String pass) throws IOException{
-        FileReader fileReader = new FileReader("C:/Java 42т/1/Registration.txt");
-        fileReader.read();
-        if (fileReader.equals(login)){
 
+    }
+    public void sign(TextField login, PasswordField pass, Label status) throws IOException{
+        FileReader fileReader = new FileReader("C:/Java_42/1/Registration.txt");
+        String str;
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ArrayList<String> arrayList = new ArrayList<>();
+        while ((str = bufferedReader.readLine()) !=null){
+            if (str.isEmpty()) break;
+            arrayList.add(str);
         }
+        ArrayList <String> arrayList1 = new ArrayList<>();
+        arrayList1.add(login.getText());
+        arrayList1.add(pass.getText());
+        if (arrayList.equals(arrayList1)){
+            status.setText("Вы успешно вошли в систему");
+        }else status.setText("Неверный логин или пароль");
+        fileReader.close();
     }
 }
