50743a72c65883e16bb04f307e24c06ad751669c
Max <gsx401@yandex.ru>
2020-10-21 09:54:03 +0300

    git remote add origin https://github.com/Artem473/-.git git branch -M main git push -u origin main

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,32 +2,27 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import java.io.IOException;
+import javafx.scene.control.Label;
 
 public class Controller {
     Model model = new Model();
 
     @FXML
     private TextField login;
-
     @FXML
     private PasswordField pass;
-
     @FXML
     private Label status;
-
     @FXML
     public void pressReg(ActionEvent event) throws IOException {
-        model.reg(login.getText(), pass.getText());
-        status.setText(login + ", Вы успешно зарегистрировались☻");
+        model.reg(login.getText(), pass.getText(),status) ;
     }
     @FXML
     public void pressIn(ActionEvent event) throws IOException{
-        model.sign(login.getText(), pass.getText());
-        status.setText(login + ", Вы успешно авторизированы♥");
+        model.sign(login ,pass,status);
+
     }
 }
